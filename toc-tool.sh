a795a41b07061af26773ed15a6bb1f5ecb9aa459
wjs <whinwy@163.com>
2023-07-28 22:35:08 +0800

    Delete comment.

--- toc-tool.sh
+++ toc-tool.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# echo $(dirname $0)
 cd $(dirname $0)
 SHELLDIR=$(pwd)
 CREATETOOLRELATIVEPATH="create-toc/pdf-toc-creator.py"
 DELETETOOLRELATIVEPATH="delete-toc"
-
-# echo $SHELLDIR
 
 arg_num=$#
 file_path=$1
@@ -15,27 +12,14 @@ toc_path=$3
 flag=$4
 
 if [[ $arg_num = 4 && "$flag" == "--delete" ]];then
-    # TODO delete toc --> create toc
-    echo $file_path
+    # delete toc --> create toc
     processed_file_path=${file_path%.pdf}_toc-deleted.pdf
-    echo $processed_file_path
-    echo $offset
-    echo $toc_path
-    echo $flag
     cd $DELETETOOLRELATIVEPATH
-    # pwd
     mvn clean compile exec:exec -Dexec.executable="java" -Dexec.args=" -classpath %classpath my.tool.pdf.App $file_path $processed_file_path" 
-    # echo mvn clean compile exec:exec -Dexec.executable="java" -Dexec.args=" -classpath %classpath my.tool.pdf.App $file_path $processed_file_path" 
     cd $SHELLDIR
-    echo $SHELLDIR
     python3 $CREATETOOLRELATIVEPATH -f $processed_file_path -o $offset -t $toc_path
     rm -fr $processed_file_path
 else
-    # TODO create toc
-    # echo $file_path
-    # echo $offset
-    # echo $toc_path
-    # echo $(pwd)
-    # echo $CREATETOOLRELATIVEPATH
+    # create toc
     python3 $CREATETOOLRELATIVEPATH -f $file_path -o $offset -t $toc_path 
 fi;
