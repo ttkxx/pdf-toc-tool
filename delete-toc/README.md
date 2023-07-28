eda12840077a94c7d54a435fa06ca36a11a7a0ba
wjs <whinwy@163.com>
2023-07-28 22:08:11 +0800

    delete-toc

---
+++ delete-toc/README.md
@@ -0,0 +1,5 @@
+# 删除指定PDF目录
+
+删除指定PDF文件的目录
+
+参考：[Removing Items from a PDF's Outline Tree](https://kb.itextpdf.com/home/it7kb/examples/removing-items-from-a-pdf-s-outline-tree)
--- delete-toc/src/main/java/my/tool/pdf/App.java
+++ delete-toc/src/main/java/my/tool/pdf/App.java
@@ -10,23 +10,41 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class App {
 
-    public static final String DEST = "/home/root/tool/pdf-toc-creator-master/ONJAVAoutlinechanged.pdf";
-    public static final String INPUT = "/home/root/tool/pdf-toc-creator-master/ONJAVA.pdf";
+    // public static final String DEST = "/home/root/tool/toc-modify/toc-modify/example/InfoQ-copy/InfoQ-2020中国技术发展白皮书_toc_toc_deleted.pdf";
+    // public static final String INPUT = "/home/root/tool/toc-modify/toc-modify/example/InfoQ-copy/InfoQ-2020中国技术发展白皮书_toc.pdf";
+
+    // private static String getProcessedFilePath(String filePath){
+    //     return String.format("%s_toc-deleted.pdf", filePath.substring(0,filePath.lastIndexOf(".pdf")));
+    // }
+
+
+
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        File file = new File(DEST);
+        System.out.println(Arrays.toString(args));
+        if(args.length!=2) throw new IllegalArgumentException(Arrays.toString(args));
+        String filePath=args[0];
+        String processedFilePath=args[1];
+        // TODO Set path of file processed.
+        // String processedFilePath=getProcessedFilePath(filePath);
+        System.out.printf("filePath: %s\n",filePath);
+        System.out.printf("processedFilePath: %s\n",processedFilePath);
+
+
+        // TODO process
+        File file = new File(processedFilePath);
         file.getParentFile().mkdirs();
-        new App().removeWholeOutlineSubtree(DEST);
-//        new removeOutlines().removeAllOutlines(DEST);
+        new App().removeWholeOutlineSubtree(filePath,processedFilePath);
     }
 
-    public void removeWholeOutlineSubtree(String DEST) throws IOException {
+    public void removeWholeOutlineSubtree(String filePath,String processedFilePath) throws IOException {
         
-        PdfReader reader = new PdfReader(INPUT);
-        PdfWriter writer = new PdfWriter(DEST);
+        PdfReader reader = new PdfReader(filePath);
+        PdfWriter writer = new PdfWriter(processedFilePath);
         PdfDocument pdfDocument = new PdfDocument(reader, writer);
         pdfDocument.getOutlines(true).removeOutline();
         pdfDocument.close();
